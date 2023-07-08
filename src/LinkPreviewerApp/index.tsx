485a16fe67fca0b16896e8d2031712dd4ee286c6
Alkan <alkanorgun@gmail.com>
2023-07-08 23:52:12 +0300

    alt text update

--- src/LinkPreviewerApp/index.tsx
+++ src/LinkPreviewerApp/index.tsx
@@ -3,7 +3,7 @@ import { LinkPreviewer } from "./LinkPreviewer"
 export const LinkPreviewerApp = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-red-100 font-bold text-2xl">
-      <LinkPreviewer url="https://github.com/microlinkhq" urlAlt="alkan">
+      <LinkPreviewer url="https://github.com/microlinkhq" urlAlt="Alternative text">
         Hover Me
       </LinkPreviewer>
     </div>
